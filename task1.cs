76b0a363304feb52c8ff5375f8628e396c55a9fd
Anilykh <alykseth@icloud.com>
2024-01-25 13:49:11 +0100

    message

--- task1.cs
+++ task1.cs
@@ -1,17 +1,133 @@
 // Your program should start at this line.
-/*
-Example:
-Move();
-Move();
-Move();
+int moveCount = 0;
+
+
+for (int i = 0; i < 37; i++)
+{
+    if (Peek())
+    {
+        Move();
+        Console.WriteLine(moveCount);
+    }
+}
 Turn();
+Console.WriteLine("Turn right");
+
 Move();
+Console.WriteLine(moveCount);
 Move();
+Console.WriteLine(moveCount);
+
 Turn();
-Turn();
-Turn();
+Console.WriteLine("Turn right");
+
+for (int j = 0; j < 36; j++)
+{
+    if (Peek())
+    {
+        Move();
+        Console.WriteLine(moveCount);
+    }
+}
+TurnLeft();
+
 Move();
-*/
+Console.WriteLine(moveCount);
+Move();
+Console.WriteLine(moveCount);
+
+TurnLeft();
+
+for (int k = 0; k < 36; k++)
+{
+    if (Peek())
+    {
+        Move();
+        Console.WriteLine(moveCount);
+    }
+}
+
+Turn();
+Console.WriteLine("Turn right");
+
+Move();
+Console.WriteLine(moveCount);
+Move();
+Console.WriteLine(moveCount);
+
+Turn();
+Console.WriteLine("Turn right");
+
+for (int l = 0; l < 36; l++)
+{
+    if (Peek())
+    {
+        Move();
+        Console.WriteLine(moveCount);
+    }
+}
+
+TurnLeft();
+
+Move();
+Console.WriteLine(moveCount);
+Move();
+Console.WriteLine(moveCount);
+
+TurnLeft();
+
+for (int m = 0; m < 36; m++)
+{
+    if (Peek())
+    {
+        Move();
+        Console.WriteLine(moveCount);
+    }
+}
+
+Turn();
+Console.WriteLine("Turn right");
+
+Move();
+Console.WriteLine(moveCount);
+Move();
+Console.WriteLine(moveCount);
+
+Turn();
+Console.WriteLine("Turn right");
+
+for (int n = 0; n < 36; n++)
+{
+    if (Peek())
+    {
+        Move();
+        Console.WriteLine(moveCount);
+    }
+}
+
+TurnLeft();
+
+Move();
+Console.WriteLine(moveCount);
+Move();
+Console.WriteLine(moveCount);
+
+TurnLeft();
+
+for (int o = 0; o < 38; o++)
+{
+    if (Peek())
+    {
+        Move();
+        Console.WriteLine(moveCount);
+    }
+    
+}
+if (AtGoal());
+    {
+        Console.WriteLine("Congratulations, you have won!");
+        return;
+    }
 
 
 #region Basic functions
@@ -21,11 +137,22 @@ Move();
 void Move()
 {
     // Moves the car 1 cell in the direction it is heading. 
+    moveCount++;
 }
 
 void Turn()
 {
     // Turns the car 90 deg clockwise.
+    
+    
+}
+void TurnLeft()
+{
+    Console.WriteLine("Turned left");
+    for (int turns = 0; turns < 3; turns++);
+    {
+        Turn();
+    }
 }
 
 bool Peek()
@@ -41,3 +168,5 @@ bool AtGoal()
 }
 
 #endregion
+
+
